22bbf4564c3172ae7535e35686f1c877af7d6c10
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-03-04 09:36:40 +0300

    refactor: change structure of the project

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import '@js';
 import '@fortawesome/fontawesome-free/js/brands';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@/assets/scss/main.scss';
+import '@/scss/main.scss';
